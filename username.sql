6febf2fc83e0b27dc9d00379117b7121b8d38abc
zpallotta <89940771+zpallotta@users.noreply.github.com>
2021-11-18 07:45:20 -0500

    Add sql command file needed for test.php

---
+++ username.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User(
+  Subject_Number INT(4) NOT NULL AUTO_INCREMENT,
+  Course_Number INT(3),
+  Course_Name CHAR(100),
+  Credits DOUBLE,
+  Mathematic_Statistics_Logic BOOLEAN,
+  Speaking BOOLEAN,
+  Writing_First_Course BOOLEAN,
+  Writing_Second_Course BOOLEAN,
+  Arts BOOLEAN,
+  Humanities BOOLEAN,
+  Natural_Science BOOLEAN,
+  Natural_Science_LAB BOOLEAN,
+  Social_Science BOOLEAN,
+  Domestic_Diversity BOOLEAN,
+  Global_Diversity BOOLEAN,
+  Capstone BOOLEAN,
+  Complex_Issues_Facing_Society BOOLEAN,
+  PRIMARY KEY (Subject_Number, Course_Number)
+);
