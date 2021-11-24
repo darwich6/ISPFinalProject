51c8fa72bd8fdb262f9a396d3c72b30dd79d6dfc
zpallotta <89940771+zpallotta@users.noreply.github.com>
2021-11-24 06:59:39 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-  <h1>
-   <span style="color:#041e42">Course</span>
-   <span style="color:#e7e2d4">Tracker</span>
-  </h1>
-  <title>Course Tracker</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <h1>
+     <span style="color:#041e42"> CS Course</span>
+     <span style="color:#e7e2d4">Tracker</span>
+    </h1>
+  <title>CS Course Tracker</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <style>  
   	* {
 		box-sizing: border-box;
@@ -66,12 +69,10 @@
 		border-radius: 4px;
 		box-sizing: border-box;
 	}
-	
-	#container {
-		bottom: auto|length|initial|inherit;
+	.container {
+	   	float: left;
+		width: 50%;
 	}
-
-	
   </style>
 </head>
 <body>
@@ -84,7 +85,7 @@
 	   <th>Course</th>
 	   <th>Name</th>
 	   <th>Credits</th>
-        </tr>
+    </tr>
 <?php
 $servername = "localhost";
 $username = "root";
@@ -126,18 +127,18 @@ if ($result-> num_rows > 0) {
 } else {
 	echo "0 Results found";
 }
-
-	echo  "</div>";
-	echo  "<div class='column'>";
-	echo   "<h2>My Courses</h2>";
-	echo   "<table id='mycourses'>";
-	echo  	"<tr>";
-	echo	   "<th>Subject</th>";
-	echo	   "<th>Course</th>";
-	echo	   "<th>Name</th>";
-	echo	   "<th>Credits</th>";
-	echo    "</tr>";
-
+?>
+  </div>
+  <div class='column'>
+  <h2>My Courses</h2>
+   <table id='mycourses'>
+    <tr>
+       <th>Subject</th>
+       <th>Course</th>
+       <th>Name</th>
+       <th>Credits</th>
+    </tr>
+<?php
 $sql = "SELECT Subject_Number, Course_Number, Course_Name, Credits FROM username";
 $result = $conn->query($sql);
 
@@ -155,6 +156,7 @@ if ($result-> num_rows > 0) {
 } else {
 	echo "0 Results found";
 }
+
 $conn->close();
 ?>
   </div>
@@ -166,7 +168,7 @@ $conn->close();
   <form action="http://localhost/isp/TermProject/test.php" name="coursetrackerform" method="post">
     <div class="form-group">
 	  <label for="subjectnumber">Subject Number:</label>
-	  <input type="text" class="form-control" id="subjectnumber" placeholder="Enter Subject" name="subjectnumber">
+	  <input type="text" class="form-control" id="subjectnumber" onkeyup="narrowList()" placeholder="Enter Subject" name="subjectnumber">
 	</div>
     <div class="form-group">
 	  <label for="coursenumber">Course Number:</label>
@@ -185,7 +187,31 @@ $conn->close();
   </form>
 </div>
 </div>
-<span id="val"></span>
+<div id="genedprogresssheet" >
+  <div id="academicfoundations">
+    <span id="mathstatslogic"></span><br>
+    <span id="speaking"></span><br>
+    <span id="writing"></span><br>
+  </div>
+  <div id="breadthofknowledge">
+    <span id="arts"></span><br>
+    <span id="humanities"></span><br>
+    <span id="natscience"></span><br>
+    <span id="socscience"></span><br>
+  </div>
+  <div id="diversity">
+    <span id="domesticdiv"></span><br>
+    <span id="globaldiv"></span><br>
+  </div>
+  <div id="integ&appliedlearning">
+    <span id="capstone"></span><br>
+    <span id="complexissues"></span><br>
+    <span id="totalcreditstaken"></span><br>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
@@ -219,13 +245,43 @@ function fillFields(row)
 	}
 }
 
-    var table = document.getElementById("mycourses"), sumVal = 0;
+function narrowList() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("subjectnumber");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("courselist");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+var table = document.getElementById("mycourses"), sumVal = 0.0;
+var mathstatslogic = speaking = writing = arts = humanities = natscience = socscience = domesticdiv = globaldiv = capstone = complexissues = 0.0;
             
-    for(var i = 1; i < table.rows.length; i++)
-    {
-        sumVal = sumVal + parseInt(table.rows[i].cells[3].innerHTML);
-    }
+for(var i = 1; i < table.rows.length; i++)
+{
+    sumVal = sumVal + parseFloat(table.rows[i].cells[3].innerHTML);
+}
             
-    document.getElementById("val").innerHTML = "Total Credits Taken = " + sumVal;
-    console.log(sumVal);
+document.getElementById("mathstatslogic").innerHTML = "Mathematics, Statistics, and Logic = " + mathstatslogic + "/3";
+document.getElementById("speaking").innerHTML = "Speaking = " + speaking + "/3";
+document.getElementById("writing").innerHTML = "Writing = " + writing + "/6";
+document.getElementById("arts").innerHTML = "Fine Arts and Humanities = " + arts + "/9";
+document.getElementById("humanities").innerHTML = "Humanities = " + humanities + "/1";
+document.getElementById("natscience").innerHTML = "Natural Science = " + natscience + "/7";
+document.getElementById("socscience").innerHTML = "Social Science = " + socscience + "/6";
+document.getElementById("domesticdiv").innerHTML = "Domestic Diversity = " + domesticdiv + "/1";
+document.getElementById("globaldiv").innerHTML = "Global Diversity = " + globaldiv + "/1";
+document.getElementById("capstone").innerHTML = "Capstone = " + capstone + "/1";
+document.getElementById("complexissues").innerHTML = "Complex Issues Facing Society = " + complexissues + "/1";
+document.getElementById("totalcreditstaken").innerHTML = "Total Credits Taken = " + sumVal + "/120";
 </script>
